0ec6c241e5e626052b4df75b68153d76dc376632
URK21CS2037 <109648094+URK21CS2037@users.noreply.github.com>
2022-10-05 11:35:21 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,17 @@
 import re
-
-def display_hash(hashtable) -> None:
-	for keyvalue in range(len(hashtable)):
-		cont = hashtable[keyvalue] 
-                print(keyvalue, end = " ") 
-                for element in cont:
-			print("-->", element, end = " ") 
-                print() 
+def display_hash(hashtable)->None:
+    for keyvalue in range(len(hashtable)):
+	   cont = hashtable[keyvalue] 
+            print(keyvalue, end = " ") 
+            for element in cont:
+		   print("-->", element, end = " ") 
+             print() 
 
 def Hashing(keyvalue) -> int:
-	return keyvalue % len(HashTable)
+    return keyvalue % len(HashTable)
 
 def insert(Hashtable, keyvalue, value) -> None:
-	Hashtable[Hashing(keyvalue)].append(value) 
+    Hashtable[Hashing(keyvalue)].append(value) 
 
 
 # Do not edit the following code
